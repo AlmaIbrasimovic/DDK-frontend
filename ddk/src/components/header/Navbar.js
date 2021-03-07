1bb21517fe930f0b78200d6e36c25c10f56517c8
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2021-03-07 01:40:04 +0100

    Prebaceno na bumlu

--- ddk/src/components/header/Navbar.js
+++ ddk/src/components/header/Navbar.js
@@ -6,6 +6,16 @@ import Button from '@material-ui/core/Button';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import logo from './logo.png';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { SidebarData } from './SidebarData';
+import SubMenu from './SubMenu';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { red } from '@material-ui/core/colors';
 
 function Navbar() {
     const [click, setClick] = useState(false)
@@ -31,15 +41,75 @@ function Navbar() {
             setDropdown (false)
         }
     }
- 
+    function clickBurger(event){
+        document.getElementsByClassName('navbar-burger')[0].classList.toggle('is-active');
+    }
+  
     return (
         <>
+        <div className = "navbar">
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navMenu" onClick={clickBurger}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
+            <div className = "navbar-brand">
+                <img src = {logo} class="image is-96x96"/>
+                <p>CK FBiH Darivanje krvi</p> 
+            </div>
+            <div className="navbar-menu" id = "navMenu">
+                <div className="navbar-end">
+                    <a className="navbar-item">Akcije darivanja krvi</a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            Info
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                Zašto darivati krv
+                            </a>
+                            <a class="navbar-item">
+                                Krvne grupe
+                            </a>
+                            <a class="navbar-item">
+                                Kako pomaže darovana krv
+                            </a>
+                            <a class="navbar-item">
+                                Proces darivanja krvi
+                            </a>
+                            <a class="navbar-item">
+                                Vodič za darivaoce krvi
+                            </a>
+                            <a class="navbar-item">
+                                Česta pitanja o darivanju krvi
+                            </a>
+                            <a class="navbar-item">
+                                Rječnik pojmova
+                            </a>
+                        </div>
+                    </div>
+                    <a className = "navbar-item">Transfuzijski centri</a>
+                    <button class="button is-danger is-outlined">
+                        <span class="icon">
+                            <i class="fas fa-user-alt"></i>
+                        </span>
+                        <span>Prijavi se</span>
+                    </button>
+                </div>
+            </div>
+            
+        </div>
+  
+        {/* 
         <nav className = "navbar">
+            <div className="navbar-div">
             <Link to = "/" className = "navbar-logo">
                 <img src = {logo} height="63" className="logo"/>
-                CK FBiH
-                Darivanje krvi
+               <p>CK FBiH
+                Darivanje krvi</p> 
             </Link>
+            </div>
+           
             <div className = "menu-icon" onClick={handleClick}>
                 <i className = {click ? "fas fa-times" : "fas fa-bars"} />
             </div>
@@ -70,11 +140,13 @@ function Navbar() {
                         <i className="fas fa-user-alt"/>
                     </a>
                 </li>
-               
+
             </ul>
-        </nav>
+        </nav>*/}
         </>
     )
+    
+    
 } 
 
 export default Navbar;
---
+++ ddk/src/components/header/burger-menu.js
@@ -0,0 +1,6 @@
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#navMenu');
+
+burgerIcon.addEventListener('click', () => {
+  navbarMenu.classList.toggle('is-active');
+});
