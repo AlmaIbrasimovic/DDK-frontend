6fb66ab63902d5d7c8ffb16f0b30e4346bd20d17
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2021-03-23 23:32:40 +0100

    Dodan kalendar akcija

--- ddk/src/pages/akcije-darivanja/AkcijeDarivanja.js
+++ ddk/src/pages/akcije-darivanja/AkcijeDarivanja.js
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, {Component,useState} from 'react';
 import Navbar from '../home-page/Navbar'
+import {
+  Calendar,
+  momentLocalizer,
+} from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment'
+import './AkcijaDarivanja.css'
+import events from "./events";
 
-function AkcijeDarivanja() {
-    return (
-      <>
-      <Navbar />
-      <h1>Akcije darivanja</h1>
+const localizer = momentLocalizer(moment);
+const messages = { 
+  allDay: 'Cijeli dan',
+  previous: 'Prethodni',
+  next: 'Sljedeći',
+  today: 'Danas',
+  month: 'Mjesec',
+  week: 'Sedmica',
+  day: 'Dan',
+  agenda: 'Agenda',
+  date: 'Datum',
+  time: 'Vrijeme',
+  event: 'Događaj'
+};
 
-      </>
-    );
-  }
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  })
   
-export default AkcijeDarivanja;
+export default class AkcijeDarivanja extends Component {
+ 
+   render() {
+       return (
+          <div className = "div-kalendar">
+            <Navbar/>
+            <Calendar   
+             classes = "kalendar"
+              toolbar={true}
+              events={events}
+              localizer={localizer}
+              startAccessor='start'
+              endAccessor='end'
+              messages={messages} // new
+              defaultDate={moment().toDate()}
+              views={['month','day','agenda']}
+              onSelectEvent={event => alert(event.title)}
+              style={{ height: 750, paddingRight:45,paddingLeft:50,paddingTop:150, }}
+              components={{
+                timeSlotWrapper: ColoredDateCellWrapper,
+              }}
+              eventPropGetter={event => {
+                const eventData = events.find(ot => ot.id === event.id);
+                const backgroundColor = eventData && eventData.color;
+                return { style: { backgroundColor } };
+              }}
+            />
+        </div>
+       )
+   }
+}
+
